ac24a633b1dd3854b51e01dd5d6ba227c7deaac0
hemachandran-k <hemachandranh868@gmail.com>
2026-04-15 10:57:50 +0530

    changes in main file

--- hedra-furniture-gallery/src/main.tsx
+++ hedra-furniture-gallery/src/main.tsx
@@ -1,3 +1,7 @@
+if (window.location.hostname.includes("onrender.com")) {
+  window.location.replace("https://www.edendek.com");
+}
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
